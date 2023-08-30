98d1a17a3753a8361be1f2a5dca53ceec1b62295
Kshitij Sharma <110044782+Kshitij210@users.noreply.github.com>
2023-08-30 11:35:06 +0530

    New Commits

---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,19 @@
+let a , b , c ;//learning to declare variables
+a = 10 , b = 12,  c = 10; //variable can be declred in a single line as well
+// ; is important and should be put after every line when you are done with the code
+
+sum = a + b + c;
+//This is actually a bad practice we must always decalre a variable before using it
+
+let sum1 = a + b ;
+
+console.log("The value of sum1 " + "= "  + sum1);
+
+console.log(sum);
+
+const intro = "The name is Kshitij" ;
+const ra = "He is interning in london right now";
+const res = intro +", "+ ra ; 
+console.log(res);
+//Basically me flirting with strings
+console.log("Kshitij" + " " + "Intern at palantir");
---
+++ 01_Basics/const.js
@@ -0,0 +1,2 @@
+const names = [1, 2, 3];
+console.log(typeof(names));
