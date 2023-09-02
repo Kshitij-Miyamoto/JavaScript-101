8cda9f958f618b6c1dbef425198c8ac7505ed2e3
Kshitij Sharma <110044782+Kshitij210@users.noreply.github.com>
2023-09-02 17:37:11 +0530

    New Commits

---
+++ 01_Basics/Strings.js
@@ -0,0 +1,23 @@
+const name = "Kshitij Sharma";
+const intern = "Palantir";
+
+//this is old and kind of outdated way of writing strings
+console.log(name + " is interning at " + intern);
+
+//this is new, efficient and better way of writing strings
+console.log(`${name} is interning at ${intern} UK`);
+
+//Accesing elements of a string
+console.log(name[0]);
+console.log(intern[2]);
+
+//.length
+console.log(name.length);
+
+//.concat
+console.log(name.concat(" urf Salu "));
+
+
+
+
+
